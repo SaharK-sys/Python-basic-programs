4da556115b8aa37bc5037c1ac68daa3945ffb86b
SaharK-sys <saharkhan9090123@gmail.com>
2025-02-18 10:51:19 +0530

    Q5
    # find smallest of 2 no.s

---
+++ Lab2/p5.py
@@ -0,0 +1,10 @@
+# to find smallest of two numbers
+a = int(input("Enter first number " ))
+b = int(input("Enter second number " ))
+if(a<b):
+    print(" First number is smaller " )
+elif(b<a):
+    print(" Second number is smaller " )
+else:
+    print(" Numbers are equal " )
+    
