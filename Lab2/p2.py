95c0eb9f0fb17442103018089bfa3d07e1e5fa84
SaharK-sys <saharkhan9090123@gmail.com>
2025-02-18 10:48:27 +0530

    Q2
    // even/ odd number

---
+++ Lab2/p2.py
@@ -0,0 +1,6 @@
+# 2. to check whether a number is even or odd
+n = int(input( " enter a number " ) )
+if( n % 2 == 0 ):
+    print(" The number is even " )
+else :
+    print( " The number is odd " )
