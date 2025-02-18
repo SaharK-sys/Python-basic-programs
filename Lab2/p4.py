9f5ab3b982a61a27d825fa8da01fe2dfba87f622
SaharK-sys <saharkhan9090123@gmail.com>
2025-02-18 10:50:29 +0530

    Q4
    # vowel/ consonant

---
+++ Lab2/p4.py
@@ -0,0 +1,8 @@
+# to check whether a character is vowel or consonant
+ch = input("Enter a character: ")
+if (ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u' or ch == 'A'
+    or ch == 'E' or ch == 'I' or ch == 'O' or ch == 'U' ):
+    print(" Character entered is a vowel " )
+else :
+    print (" Character entered is a consonant ")
+    
