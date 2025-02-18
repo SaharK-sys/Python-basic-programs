4ed9aeb840e4b9042798b65665fecf271843a2a2
SaharK-sys <saharkhan9090123@gmail.com>
2025-02-18 11:04:51 +0530

    Q2
    # multiply all items in a list

---
+++ Lab6/6.2.py
@@ -0,0 +1,8 @@
+# 6.2 to multiply all items in a list
+numbers = [25, 50, 75]
+product = 1
+for num in numbers:
+    product *= num
+print("list is ",numbers)
+print("product of given numbers is",product)
+
