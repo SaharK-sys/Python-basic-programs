e504309ce1504b8687e3846b99bce1a893eff88e
SaharK-sys <saharkhan9090123@gmail.com>
2025-02-18 11:10:58 +0530

    Q7
    # remove duplicates from a list

---
+++ Lab6/6.7.py
@@ -0,0 +1,6 @@
+#6.7 to remove duplicates from a list
+List = [7 , 9, 8, 7, 7 , 10, 1, 1, 2, 2, 2, 5, 15 ]
+noduplicatelist = list(set(List))
+print("list is ",List)
+print("List without duplicate is : ",noduplicatelist)
+
