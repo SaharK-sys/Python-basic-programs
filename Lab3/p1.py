6c9c130c0f981c8673e73eafc62c70dc510a494b
SaharK-sys <saharkhan9090123@gmail.com>
2025-02-18 10:54:07 +0530

    Q1
    # swap 2 no.s using third variable

---
+++ Lab3/p1.py
@@ -0,0 +1,14 @@
+# swap 2 numbers using a third variable
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("first number:", a)
+print("second number:", b)
+temp = a
+a = b
+b = temp
+print("first number after swapping: ", a)
+print("second number after swapping: ", b)
+
+
+
+
