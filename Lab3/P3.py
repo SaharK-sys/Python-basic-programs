11942f2d7f822952998ce14a1095b46d0a75f326
SaharK-sys <saharkhan9090123@gmail.com>
2025-02-18 10:57:58 +0530

    Q3
    # find sum of cubes of 2 no.s

---
+++ Lab3/P3.py
@@ -0,0 +1,15 @@
+# read 2 numbers and print sum of their cubes
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("first number:", a)
+print("second number:", b)
+c1 = a * a * a
+c2 = b * b * b
+sum = c1 + c2
+print("sum of cubes of 2 numbers : ", sum)
+
+
+
+
+
+
