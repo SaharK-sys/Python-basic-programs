84919896fc555a0e711585cf6aa6fdb17b93d63e
SaharK-sys <saharkhan9090123@gmail.com>
2025-02-25 00:08:13 +0530

    Q2
    # to demonstrate built- in functions in strings

---
+++ Lab4/4.2.py
@@ -0,0 +1,41 @@
+# to demonstrate built- in functions in strings
+x = "Coding in Python is fun"
+# length
+print(len(x))
+# uppercase, lowercase, capitalize, swapcase & title
+print(x.upper())
+print(x.lower())
+print(x.capitalize())
+print(x.swapcase())
+print(x.title())
+# replace
+print(x.replace("Coding","Programming"))
+print(x)
+#  concatenate
+y = "Hello"
+z = x + y
+print(z)
+# count
+print(x.count("i",0,15))
+# split
+print(x.split('in'))
+# isalpha
+print(x.isalpha())
+# isalnum
+print(x.isalnum())
+# isdigit
+print(x.isdigit())
+# isupper
+print(x.isupper())
+# islower
+print(x.islower())
+# find(“is”)
+print(x.find("is"))
+# strip
+print(x.strip())
+
+
+
+      
+
+
