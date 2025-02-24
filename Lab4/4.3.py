afdb20c136c2724544bfa498f12dd3907a83c3a3
SaharK-sys <saharkhan9090123@gmail.com>
2025-02-25 00:19:31 +0530

    Q3
    # to check weather a given string is palindrome or not

---
+++ Lab4/4.3.py
@@ -0,0 +1,12 @@
+# to check weather a given string is palindrome or not.
+string = input("Enter a string")
+if(string == string[::-1]):
+    print("It is a palindrome")
+else:
+    print("Not a palindrome")
+
+
+
+      
+
+
