fcb852e180f60c7cb9efb7f007ab92db3cd6081c
SaharK-sys <saharkhan9090123@gmail.com>
2025-02-25 11:08:39 +0530

    Q2
    # Print 1st 5 even numbers (use break statement)

---
+++ Lab5/5.2.py
@@ -0,0 +1,16 @@
+# to Print 1st 5 even numbers (use break statement).
+num = 0
+count = 0
+print("even numbers are:")
+while True:
+    if (num % 2==0):
+        print(num)
+        count +=1
+    if count == 5:
+        break
+    num += 1
+
+    
+    
+    
+    
