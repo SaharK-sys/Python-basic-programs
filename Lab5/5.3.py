ec969d50376a5fc4b5be97804464e8cbecc6ea84
SaharK-sys <saharkhan9090123@gmail.com>
2025-03-13 21:10:47 +0530

    Q3
    # Print 1st 4 even numbers (use continue statement).

---
+++ Lab5/5.3.py
@@ -0,0 +1,16 @@
+# Print 1st 4 even numbers (use continue statement).
+n = -1
+count = 0
+print("even numbers are:")
+while count < 4:
+    n +=1 
+    if (n % 2!=0):
+        continue
+    print(n)
+    count +=1
+    
+   
+    
+    
+    
+    
