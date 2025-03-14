7e39ffe1fe5b5d05562469366048c08da40620b5
SaharK-sys <saharkhan9090123@gmail.com>
2025-03-14 23:12:50 +0530

    Q13
    # program to slice a tuple

---
+++ Lab7/7.13.py
@@ -0,0 +1,8 @@
+# program to slice a tuple
+example_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+slice1 = example_tuple[-4:-1]
+slice2 = example_tuple[::2]
+slice3 = example_tuple[::-1]
+print(slice1)
+print(slice2)
+print(slice3)
