53ba5cb23110e27ca033b7d33d4eff1b58e69da9
SaharK-sys <saharkhan9090123@gmail.com>
2025-03-14 23:30:25 +0530

    Q10
    # Create a simple calculator

---
+++ Lab2/p10.py
@@ -0,0 +1,9 @@
+# Simple calculator
+n1 = int(input("Enter first number"))
+n2 = int(input("Enter second number"))
+print("addition ", n1 + n2)
+print("subtraction ", n1 - n2)
+print("multiplication ", n1 * n2)
+print("division ", n1 / n2)
+
+
