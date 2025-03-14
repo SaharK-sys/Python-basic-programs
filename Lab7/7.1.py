387580a0361cc5f36b453221fcc31c89899bb5f0
SaharK-sys <saharkhan9090123@gmail.com>
2025-03-14 18:13:53 +0530

    Q1
    # program to create a tuple

---
+++ Lab7/7.1.py
@@ -0,0 +1,9 @@
+# to create a tuple
+tuple1= (1,2,3,4)
+tuple2 = (1, "a", "hello", True)
+input_tuple = input("Enter values separated by comma")
+tuple3 = tuple(input_tuple.split(","))
+print(tuple1)
+print(tuple2)
+print(tuple3)
+
