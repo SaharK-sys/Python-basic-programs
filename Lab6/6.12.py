d0e5a6da4bf6bbe80b2fd79b026c4cd0b7a4527c
SaharK-sys <saharkhan9090123@gmail.com>
2025-03-16 14:33:24 +0530

    Q12
    # program to print a specified list after removing the 0th, 4th and 5th elements

---
+++ Lab6/6.12.py
@@ -0,0 +1,8 @@
+# to print a specified list after removing 0th, 4th & 5th elements.  
+colors = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+del colors[5]
+del colors[4]
+del colors[0]
+print(colors)
+
+
